41df9f14ecc926ab8f0c6181cc7d49e6229a0b5d
9-box <84057145+9-box@users.noreply.github.com>
2022-03-17 12:25:06 +0900

    Update if_else_elseif.php

--- if_else_elseif.php
+++ if_else_elseif.php
@@ -2,13 +2,23 @@
 	$n = 27;
 
 	if ($n%2==0) {
-	 	echo "$n : 짝수";
+	 	echo "$n : 짝수<br>";
 	 } 
 	else{
-		echo "$n : 홀수";
+		echo "$n : 홀수<br><br>";
 	}
 /*if (condition) {
 	// code...
 }
 if 기본 문법*/
+	echo "다른 예제<br>";
+	$age = 66;
+	$fee = "5000원";
+
+	if ($age >= 65) {
+		
+		$fee = "무료";// code...
+	}
+	echo "나이 : $age 세<br>";
+	echo "입장료 : $fee";
 ?>
