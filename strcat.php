a5f859d8368f641557cbd842bec0b9f145f4c2f6
9-box <84057145+9-box@users.noreply.github.com>
2022-03-17 11:32:41 +0900

    Add files via upload

---
+++ strcat.php
@@ -0,0 +1,16 @@
+<?php
+	$num1 = "010";
+	$num2 = "1234";
+	$num3 = "5678";
+	$phone_number = $num1."-".$num2."-".$num3;
+	//문자열 합칠시 $변수명.$변수명으로 합침
+	echo "휴대폰 번호 : $phone_number"."<br>";
+
+	//복습
+	$email1 =  "admin";
+	$email2 =  "naver.com";
+	$email =  "$email1"."@"."$email2";
+
+	echo "이메일 주소 : $email";
+
+?>
